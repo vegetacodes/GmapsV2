4afbe6d9c852a08c72739dc21d79a9311809cf0e
Uday Kadaboina <uday88k@gmail.com>
2012-11-04 02:10:02 -0600

    User enterable geo-cordinates transformed as markers on map

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,12 @@
 class Issue < ActiveRecord::Base
   attr_accessible :address, :description, :latitude, :longitude, :title
+
+ acts_as_gmappable :process_geocoding => false
+      def gmaps4rails_address
+          address
+      end
+       def gmaps4rails_infowindow
+         "<h4>#{title}</h4>" << "<h4>#{address}</h4>"
+     end
+
 end
